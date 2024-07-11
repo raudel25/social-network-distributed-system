0b8b1dd986ae043e2a761b7330a0cd08eb74db24
Anabel Benítez González <anabelbenitezglez@gmail.com>
2024-07-11 08:57:53 -0400

    Start with services

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"flag"
-
-	"github.com/raudel25/social-network-distributed-system/pkg/chord"
+	socialnetwork "github.com/raudel25/social-network-distributed-system/internal/services"
 	"github.com/raudel25/social-network-distributed-system/pkg/logging"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	logging.SettingLogger(log.DebugLevel, ".")
-	port := flag.String("p", "5000", "Default port is 5000")
-	broad := flag.String("b", "8000", "Default port broad is 8000")
-	flag.Parse()
+	rsaPrivateKeyPath := "pv.pem"
+	rsaPublicteKeyPath := "pub.pem"
+	network := "tcp"
 
-	config := chord.DefaultConfig()
-	storage := chord.NewRamStorage()
+	socialnetwork.Start(rsaPrivateKeyPath, rsaPublicteKeyPath, network)
 	
-	node := chord.NewNode(config, storage)
-
-	node.Start(*port, *broad)
-
 	for {
 	}
 
